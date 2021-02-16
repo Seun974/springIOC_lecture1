5420af40ec921354400b830b47d8ad93d01e30ce
Seun Bond <sadetoye@gmail.com>
2021-02-17 00:22:42 +0100

    Initial commit. inversion IOC configured with XML and beans done too

--- src/main/java/se/lexicon/samuel/App.java
+++ src/main/java/se/lexicon/samuel/App.java
@@ -1,9 +1,33 @@
 package se.lexicon.samuel;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.lexicon.samuel.data.CourseDAO;
+import se.lexicon.samuel.data.StudentDAO;
+import se.lexicon.samuel.model.Course;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class App {
     public static void main( String[] args ) {
 
-        System.out.println("Hello World!");
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CourseDAO courseDAO = context.getBean(CourseDAO.class);
+        StudentDAO studentDAO = context.getBean(StudentDAO.class);
+
+        //declaring the variable
+        String id = UUID.randomUUID().toString();
+
+        courseDAO.persist(
+                new Course(id, "Java", LocalDate.parse("2021-03-15"), LocalDate.now().plusMonths(4), null)
+        );
+
+        System.out.println(courseDAO.findById(id));
+
+        context.close();
+
     }
 }
