13b9ab02b91a2a4d02e65c2677d33bf93b0c74f4
Seun Bond <sadetoye@gmail.com>
2021-02-23 23:39:13 +0100

    master with applicationContext.xml added.

--- src/main/java/se/lexicon/samuel/App.java
+++ src/main/java/se/lexicon/samuel/App.java
@@ -1,32 +1,10 @@
 package se.lexicon.samuel;
 
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import se.lexicon.samuel.config.ApplicationConfiguration;
-import se.lexicon.samuel.data.CourseDAO;
-import se.lexicon.samuel.data.StudentDAO;
-import se.lexicon.samuel.model.Course;
-
-import java.time.LocalDate;
-import java.util.UUID;
 
 public class App {
     public static void main( String[] args ) {
 
-//        AnnotationConfigApplicationContext context =
-//                new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-//
-//        StudentDAO studentDAO = context.getBean(StudentDAO.class);
-//        CourseDAO courseDAO = context.getBean(CourseDAO.class);
-//
-//        String id = UUID.randomUUID().toString();
-//
-//        //to find them
-//        courseDAO.persist(new Course(id, "C#", LocalDate.now(), LocalDate.now().plusMonths(8), null));
-//        System.out.println(courseDAO.findById(id));
-//
-//
-//
-//        context.close();
+
     }
 }
